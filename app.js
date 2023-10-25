138501177d86618af8cd0266055d31cb10aca40c
Sclilly93 <Sethlilly93@gmail.com>
2023-10-24 20:41:32 -0400

    1.5

--- app.js
+++ app.js
@@ -39,16 +39,90 @@ app.use('/suits', suitRoutes);
 app.use('/suits', suitOptionsRoutes);
 
 
-
 //  bodyParserMiddleware.js code setup for import and use
 const parseRequestBody = require('./middleware/bodyParserMiddleware'); // Adjust the path as needed
-
 
 // Use the bodyParser middleware for parsing incoming requests
 app.use(parseRequestBody());
 app.use(bodyParser.json());
 
+
+
+
+
 // Other middleware and routes...
+// Define root route
+app.get('/', (req, res) => {
+  res.send('Hello NODE API')
+})
+
+// Retrieve all products
+app.get('/suitOptions', async(req, res) => {
+  try {
+      const products = await Product.find({});
+      res.status(200).json(products);
+  } catch (error) {
+      res.status(500).json({message: error.message})
+  }
+})
+
+// Retrieve a specific product by ID
+app.get('/suitOptions/:id', async(req, res) =>{
+  try {
+      const {id} = req.params;
+      const product = await Product.findById(id);
+      res.status(200).json(product);
+  } catch (error) {
+      res.status(500).json({message: error.message})
+  }
+})
+
+// Create a new product
+app.post('/suitOptions', async(req, res) => {
+  try {
+      const product = await Product.create(req.body)
+      res.status(200).json(product);
+      
+  } catch (error) {
+      console.log(error.message);
+      res.status(500).json({message: error.message})
+  }
+})
+
+// Update a product by ID
+app.put('/products/:id', async(req, res) => {
+  try {
+      const {id} = req.params;
+      const product = await Product.findByIdAndUpdate(id, req.body);
+      // we cannot find any product in database
+      if(!product){
+          return res.status(404).json({message: `cannot find any product with ID ${id}`})
+      }
+      const updatedProduct = await Product.findById(id);
+      res.status(200).json(updatedProduct);
+      
+  } catch (error) {
+      res.status(500).json({message: error.message})
+  }
+})
+  
+// Delete a product by ID
+app.delete('/suitOptions/:id', async(req, res) =>{
+  try {
+      const {id} = req.params;
+      const product = await Product.findByIdAndDelete(id);
+      if(!product){
+          return res.status(404).json({message: `cannot find any product with ID ${id}`})
+      }
+      res.status(200).json(product);
+      
+  } catch (error) {
+      res.status(500).json({message: error.message})
+  }
+})
+
+
+
 app.post('/suitOptions', (req, res) => {
     console.log(request.body);
 })
